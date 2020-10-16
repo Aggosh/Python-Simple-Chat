5b58e49bd1c1ed7c0d9fe383d6d6fa1e8cc5de20
Aggosh <bkmzilia@gmail.com>
2020-10-16 18:50:03 +0300

    Update config_server.py

--- config_server.py
+++ config_server.py
@@ -4,7 +4,7 @@ MAX_CONNECTIONS = 2
 HOST = "127.0.0.1"
 PORT = 80
 
-DATABASE = "d6lncqf4v30hkv"
-DB_USER = "arirjqnjgkavzq"
-DB_PASSWORD = "ec94ede70ebf8a80d4d96d6a6abcc20991bb0efd1f29766a9f141e6d1b93ee3c"
-DB_HOST = "ec2-50-17-197-184.compute-1.amazonaws.com"
+DATABASE = ""
+DB_USER = ""
+DB_PASSWORD = ""
+DB_HOST = ""
